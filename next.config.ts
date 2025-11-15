2de2b1ddd7540327ed4aa9d5d79527520a185c0e
Mohammad Zunaid <mohammadzunaid83@gmail.com>
2025-11-15 06:35:21 +0000

    https://ibb.co/XrZ58tW0 use this image as cover (_for element div_)

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,8 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https'
+        ,
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
@@ -37,6 +38,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
         port: '',
         pathname: '/**',
       }
