f485b83fb0000d0b963590dc6a03ad2e6cc624a9
Mohammad Zunaid <mohammadzunaid83@gmail.com>
2025-11-22 10:19:24 +0000

    use this as favicon https://aicdn.picsart.com/364d49a4-c0c5-41c4-b99e-4e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="https://aicdn.picsart.com/364d49a4-c0c5-41c4-b99e-4e0fe8f46070.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
